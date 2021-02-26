da012359bb1ae84f7c99f57f3965ad72490f18f1
mseo.39 <a01063294905@gmail.com>
2021-02-26 23:15:52 +0900

    test

---
+++ python_basic/print.py
@@ -0,0 +1,12 @@
+#출력하기
+print("hello")
+#공백을 포함한 문장 출력
+print("hello world")
+print("hello","world")
+#기본 print는 문장을 출력한 후 마지막에 줄을 마꾼다(new line)
+print("hello")
+print("world")
+#작은 따옴표가 들어있는 출력문
+print("'hello'")
+#큰 따옴표가 들어있는 출력문
+print('"hello"')
