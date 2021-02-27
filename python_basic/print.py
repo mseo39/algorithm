62c5778df0e10a849b60e41917ff8e337e54865a
mseo.39 <a01063294905@gmail.com>
2021-02-27 23:48:05 +0900

    print

--- python_basic/print.py
+++ python_basic/print.py
@@ -10,3 +10,12 @@ print("world")
 print("'hello'")
 #큰 따옴표가 들어있는 출력문
 print('"hello"')
+
+#6006번 출력형식에 필요한 따옴표와 출력할 문자인 구분하기 위하여 \'와 \"응 리용해 출력
+print('\"@#$%^&*()\'')
+
+#6007번
+print('"C:\Download'+'\\'+'\'hello\'.py"')
+
+#6008번
+print('print("Hello\nworld")')
