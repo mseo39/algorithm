8d9260e29701c17d68f092f06bc8b6b7ba5a37bb
mseo39 <a01063294905@likelion.org>
2021-08-31 22:28:04 +0900

    21.8.31

---
+++ python_algorithm/step7/10809.py
@@ -0,0 +1,26 @@
+#알파벳 찾기
+#한 단어에서 각 알파벳이 처음 등장하는 위치를 찾는 문제
+'''
+입력
+첫째 줄에 단어 S가 주어진다. 단어의 길이는 100을 넘지 않으며, 알파벳 소문자로만 이루어져 있다.
+출력
+
+'''
+from string import ascii_lowercase
+alpha_list = list(ascii_lowercase)
+n=input()
+
+for i in alpha_list:
+    if i in n:
+        alpha_list[alpha_list.index(i)]=n.index(i)
+    else:
+        alpha_list[alpha_list.index(i)]=-1
+print(*alpha_list)
+
+'''
+다른방법
+string = input()
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+for i in alphabet:
+    print(string.find(i), end = ' ')
+'''
---
+++ python_algorithm/step7/1157.py
@@ -0,0 +1,25 @@
+#단어 공부
+#주어진 단어에서 가장 많이 사용된 알파벳을 출력하는 문제
+'''
+입력
+첫째 줄에 알파벳 대소문자로 이루어진 단어가 주어진다. 주어지는 단어의 길이는 1,000,000을 넘지 않는다.
+출력
+첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다. 단, 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력한다.
+'''
+string=input().upper()
+a={}
+for i in string:
+    if i in a:
+        a[i]+=1
+    else:
+        a[i]=1
+max=max(a.values())
+cnt=0
+for key, value in a.items():
+    if value==max:
+        cnt+=1
+        n=key
+if cnt==1:
+    print(n)
+else:
+    print("?")
