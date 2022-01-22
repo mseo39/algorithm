9280aeca946a725b2ab12cdee19e79a29edb90e5
mseo39 <a01063294905@likelion.org>
2022-01-22 23:55:55 +0900

    22.01.22

--- python_algorithm1of2/200/1874.py
+++ python_algorithm1of2/200/1874.py
@@ -16,14 +16,21 @@
 import sys
 from unittest import result
 I=sys.stdin.readline
-n=int(I())
 
+# 첫 줄에 n이 주어진다
+n=int(I())
+# n개의 숫자를 입력받는다
 list_=list(int(I()) for _ in range(n))
+# 1~n을 가진 리스트를 생성
 list1_=list(i for i in range(1,n+1))
 
+# 스택을 이용하여 만들어 낼 수열에 대한 인덱스
 index_=0
+# 1~n 리스트에 사용될 인덱스
 index1_=0
+# +,- 결과를 저장할 리스트
 result_=[]
+# 스택 역할을 할 리스트
 stack_=[0,]
 '''
 1. 스택 상단에 있는 요소와 입력된 배열,
@@ -33,12 +40,22 @@ stack_=[0,]
 
 그렇다면 수열이 안만들어지는 조건이 무엇일까....
 
+아직 완성되지않는 리스트와 남은 스택에 있는 값들이 같다면
+수열이 되는거고
+같지 않으면 수열이 될 수 없는 것이다
 '''
-
+# index1이 n이 될 때까지 
+# 즉, 1~n 요소들이 스택에 전부 push 되었을 때 
 while(index1_!= n):
+    # 스택상단에 있는 값이
+    # 만들고자 한 리스트의 인덱스가 index_인 요소와
+    #값이 같다면
     if(list_[index_]==stack_[-1]):
+        #값을 꺼낸다
         stack_.pop()
+        # -을 삽입
         result_.append("-")
+        # 인덱스를 증가시킨다
         index_+=1
         continue
 
