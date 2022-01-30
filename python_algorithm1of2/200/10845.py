c6b81d5af6ee2dff089f4085c86ac4f4d5018050
mseo39 <a01063294905@likelion.org>
2022-01-30 22:00:30 +0900

    22.01.30

--- python_algorithm1of2/200/10845.py
+++ python_algorithm1of2/200/10845.py
@@ -10,11 +10,64 @@ size: 큐에 들어있는 정수의 개수를 출력한다.
 empty: 큐가 비어있으면 1, 아니면 0을 출력한다.
 front: 큐의 가장 앞에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
 back: 큐의 가장 뒤에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+
+파이썬은 큐에 대한 모듈을 제공한다
+이것을 시용하기 위하여
+ form collections import deque
+ deq=deque()
+ 
+ -deque에 존재하는 메서드
+
+ 요소를 추가할 때
+ (1) 왼쪽 끝에 삽입한다
+ deq.appendleft(item)
+ (2) 오른쪽 끝에 삽입한다
+ deq.append(item)
+
+ 요소를 삭제할 때
+ (1) 오른쪽 끝 요소를 삭제할 때
+ deq.pop()
+ (2) 왼쪽 끝 요소를 삭제할 때
+ deq.popleft()
+
+배열을 추가할 때
+(1) 주어진 배열을 순환하면서 데크의 오른쪽에 추가
+deq.extend(array)
+(2) 주어진 배열을 순환하면서 데크의 왼쪽에 추가
+deq.extendleft(array)
+
+요소 삭제
+deq.remove(item)
+데크를 num만큼 회전한다
+deq.rotate(num)
 '''
+from collections import deque
 class queue:
     def __init__(self):
-        self.queue_list=[]
+        self.deq=deque()
+    
     def push(self,item):
-        self.queue_list.append(item)
+        self.deq.appendleft(item)
     def pop(self):
-        self.pop(-1)
+        if(self.empty()):
+            print(-1)
+        else:
+            self.deq.pop()
+    def size(self):
+        print(len(self.deq))
+
+    def empty(self):
+        if(len(self.deq)==0):
+            return 1
+        else:
+            return 0
+    def front(self):
+        if(self.empty()):
+            print(-1)
+        else:
+            print(self.deq[0])
+    def back(self):
+        if(self.empty()):
+            print(-1)
+        else:
+            print(self.deq[-1])
