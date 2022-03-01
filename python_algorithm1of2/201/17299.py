0633e8a5edbbdd4d282e85dc6442d57b556a0dd4
mseo39 <a01063294905@likelion.org>
2022-03-01 23:33:29 +0900

    22.03.01

--- python_algorithm1of2/201/17299.py
+++ python_algorithm1of2/201/17299.py
@@ -10,6 +10,14 @@ Ai가 수열 A에서 등장한 횟수를 F(Ai)라고 했을 때, Ai의 오등큰
 Ai의 오른쪽에 있으면서 등장한 횟수가 3보다는 큰 수는 없기 때문에, NGF(1)=-1이다
 A3의 경우에는 A7이 오른쪽에 있으면서 F(A3=2)<F(A7=1)이기 떄문에,
 NGF(3)=1이다. NGF(4)=2, NGF(5)=2, NGF(6)=1 이다.
+
+1. 먼저 각 숫자들이 몇개 씩 있는지 확인을 한 리스트를 생성한다
+1 1 2 3 4 2 1
+3 3 2 1 1 2 3
+2. 각 자리들의 오른쪽에 자신보다 큰 숫자가 몇개있는지 출력해야 한다
+없으면 -1을 출력한다
+
+'''
 '''
 #첫째줄에 수열 A의 크기N이 주어진다
 N=int(input())
@@ -23,3 +31,27 @@ for i in range(len(A)):
         result_[stack_.pop()]=A[i]
     stack_.append(i)
 print(*result_)
+'''
+
+# counter 함수는 내가 찾는 문자의 개수를 딕셔너리형태로 저장해주는 함수다
+from collections import Counter
+
+#첫째줄에 수열 A의 크기N이 주어진다
+n = int(input())
+#리스트를 받는다
+data = list(map(int, input().split()))
+count = Counter(data) # 변수 마다 개수를 저장
+print(count)
+#빈 스택을 생성한다
+stack = []
+# -1 값을 가지고 있는 리스트를 생성한다
+result = [-1] * n
+stack.append(0)
+
+for i in range(n):
+    while stack and count[data[stack[-1]]] < count[data[i]]:
+        result[stack.pop()] = data[i]
+    stack.append(i)
+
+for r in result:
+    print(r, end=' ')
